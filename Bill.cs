816ad7a5d26b083e3f63b83ac2b757978093e5c5
moclaw <56180480+moclaw@users.noreply.github.com>
2021-05-05 01:34:59 +0700

    Start

---
+++ Bill.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class Bill : Bills
+    {
+        public string IdCustomer, Address, Name, ID;
+        public double Phone;
+        public int day, mounth, year;
+        public override void InputBill()
+        {
+            Console.Write("Ma hoa don: ");
+            ID = Console.ReadLine();
+            Console.Write("Ngay lap: ");
+            day = int.Parse(Console.ReadLine());
+            Console.Write("Thang lap: ");
+            mounth = int.Parse(Console.ReadLine());
+            Console.Write("Nam lap: ");
+            year = int.Parse(Console.ReadLine());
+        }
+
+        public override void InputInfor()
+        {
+
+            Console.Write("Ma khach hang: ");
+            IdCustomer = Console.ReadLine();
+            Console.Write("Ten khach hang: ");
+            Name = Console.ReadLine();
+            Console.Write("Dia chi: ");
+            Address = Console.ReadLine();
+            Console.Write("So dien thoai: ");
+            Phone = double.Parse(Console.ReadLine());
+        }
+        public string BilltoString() => $"Hoa Don: {ID}\t{day}/{mounth}/{year}\n";
+        public string InfortoString() => $"Thong tin khach hang: {IdCustomer}\t{Name}\t{Address}\t{Phone}\n";
+    }
+}
---
+++ DetailBill.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP
+{
+    class DetailBill : DetailBills
+    {
+        public string IdDevices, Devices, NameDevices, Company, OptionDevices, CoolMachine, DetailsCool1, DetailsCool2;
+        public int  WaterCapacity, Amout, Total1, Total2, TotalBill, Electric;
+        public override void inputDetailsBill()
+        {
+            Console.Write("Chon loai thiet bi (1 - may quat,  2 - may lanh): ");
+            Devices = Console.ReadLine();
+            Console.Write("Nhap ma: ");
+            IdDevices = Console.ReadLine();
+            Console.Write("Ten san pham: ");
+            NameDevices = Console.ReadLine();
+            Console.Write("Noi san xuat: ");
+            Company = Console.ReadLine();
+            Console.Write("So luong ban ra: ");
+            Amout = int.Parse(Console.ReadLine());
+
+            if (Devices == "1")
+            {
+                Devices = "may quat";
+                Console.Write("Chon loai quat (1 - may quat dung, 2 - may qua hoi nuoc, 3 - may quat sac dien): ");
+                OptionDevices = Console.ReadLine();
+                if (OptionDevices == "1")
+                {
+                    OptionDevices = "May quat dung";
+                    Total1 = Amout * 500;
+                }
+                else if (OptionDevices == "2")
+                {
+                    OptionDevices = "May quat hoi nuoc";
+                    Console.Write("Dung tich nuoc: ");
+                    WaterCapacity = int.Parse(Console.ReadLine());
+                    Total1 = Amout * (WaterCapacity * 400);
+                }
+                else if (OptionDevices == "3")
+                {
+                    OptionDevices = "May quat sac dien";
+                    Console.Write("Nhap dung luong pin: ");
+                    Electric = int.Parse(Console.ReadLine());
+                    Total1 = Amout * (Electric * 500);
+                }
+            }
+            else if (Devices == "2")
+            {
+                Devices = "may lanh";
+                Console.Write("Chon loai may lanh (1 - 1 chieu, 2 - 2 chieu): ");
+                OptionDevices = Console.ReadLine();
+                if (OptionDevices == "1")
+                {
+                    OptionDevices = "1 Chieu";
+                    Console.Write("Co cong nghe inverter khong (1 - khong co, 2 - co)");
+                    CoolMachine = Console.ReadLine();
+                    if (CoolMachine == "1")
+                    {
+                        Total2 = Amout * 1000;
+                    }
+                    else
+                    {
+                        Total2 = Amout * 1500;
+                    }
+                }
+                else if (OptionDevices == "2")
+                {
+                    OptionDevices = "2 Chieu";
+                    Console.Write("Co cong nghe inverter khong (1 - khong co, 2 - co)");
+                    CoolMachine = Console.ReadLine();
+                    if (CoolMachine == "1")
+                    {
+                        CoolMachine = "Co cong nghe inveter";
+                        Console.Write("Co cong nghe khang khuan khong (1 - co, 2 - khong): ");
+                        DetailsCool1 = Console.ReadLine();
+                        if (DetailsCool1 == "1")
+                        {
+                            Console.Write("Co cong nghe khu mui khong (1 - co, 2 - khong: ");
+                            DetailsCool2 = Console.ReadLine();
+                            if (DetailsCool2 == "1")
+                            {
+
+                                DetailsCool2 = "Co cong nghe khu mui";
+                                Total2 = Amout * 2000 + 1000;
+                            }
+                            else
+                            {
+
+                                DetailsCool2 = "khong co cong nghe khu mui";
+                                Total2 = Amout * 2000 + 500;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        CoolMachine = "Khong co cong nghe inveter";
+                        Console.Write("Co cong nghe khu mui khong (1 - co, 2 - khong: ");
+                        DetailsCool2 = Console.ReadLine();
+                        if (DetailsCool2 == "1")
+                        {
+                            DetailsCool2 = "Co cong nghe khu mui";
+                            Total2 = Amout * 2500 + 1000;
+                        }
+                        else
+                        {
+
+                            DetailsCool2 = "khong co cong nghe khu mui";
+                            Total2 = Amout * 2500;
+                        }
+                    }
+                }
+               
+                TotalBill = Total1 + Total2;
+            }
+        }
+        public string outputDetailsBilltoString()
+        {
+            if (Devices == "may quat")
+            {
+                return $"May quat: {IdDevices}\t{OptionDevices}\t{NameDevices}\t{Company}\t{Total1}\t{Electric}\t{WaterCapacity}\t{Amout}\n\nTong don: {TotalBill}";
+            }
+            else
+            {
+                return $"May quat: {IdDevices}\t{OptionDevices}\t{NameDevices}\t{Total2}\t{CoolMachine}\t{DetailsCool1}\t{DetailsCool2}\t{Amout}\n\nTong don: {TotalBill}";
+            }
+        }
+    }
+}
